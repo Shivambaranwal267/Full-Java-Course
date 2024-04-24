414d034c045626816f547957e2bc7fc460843895
Shivam Baranwal <shivambaranwal267@gmail.com>
2024-04-24 18:28:09 +0530

    add

--- DFS.java
+++ DFS.java
@@ -1,34 +1,32 @@
 import java.util.ArrayList;
 
 public class DFS {
-     // Function to return a list containing the DFS traversal of the graph.
+    // Function to return a list containing the DFS traversal of the graph.
     public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
         // Code here
-        
+
         ArrayList<Integer> ans = new ArrayList<>();
         boolean[] vis = new boolean[V];
-        
+
         ans.add(0);
         vis[0] = true;
-        
-        dfsOfGraphHelper(adj, 0, ans,vis);
+
+        dfsOfGraphHelper(adj, 0, ans, vis);
         return ans;
     }
-    
-    public void dfsOfGraphHelper(ArrayList<ArrayList<Integer>> adj, int src, ArrayList<Integer> ans,boolean[] vis){
-           
-        
+
+    public void dfsOfGraphHelper(ArrayList<ArrayList<Integer>> adj, int src, ArrayList<Integer> ans, boolean[] vis) {
+
         ArrayList<Integer> nbrs = adj.get(src);
-        
-        for(int v: nbrs){
-            if(vis[v] == false){
+
+        for (int v : nbrs) {
+            if (vis[v] == false) {
                 ans.add(v);
                 vis[v] = true;
-                dfsOfGraphHelper(adj, v,ans,vis);
+                dfsOfGraphHelper(adj, v, ans, vis);
             }
         }
-        
-        
+
     }
 
 }
